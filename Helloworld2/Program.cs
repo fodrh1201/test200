05668479003fb1b2a3f09c5e155735f2ad58a3f1
fodrh <fodrh1201@gmail.com>
2014-03-07 09:04:28 +0900

    Xamarin second Practice

---
+++ Helloworld2/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Helloworld2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			int i = 4;
+			Console.WriteLine (i);
+		}
+	}
+}
